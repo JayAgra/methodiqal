b43c49b707902b0a59c7db21df604a33b514cfb6
JayAgra <jay.agrawal@icloud.com>
2025-07-26 01:16:05 -0400

    will do later™

---
+++ methodiqal/methodiqalApp.swift
@@ -0,0 +1,17 @@
+//
+//  methodiqalApp.swift
+//  methodiqal
+//
+//  Created by Jayen Agrawal on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct methodiqalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
